319aa3aea7ef5548c7906c1fbae01e57b571d7c9
JoaoVarela1 <84931110+JoaoVarela1@users.noreply.github.com>
2021-05-27 15:02:34 -0300

    Add files via upload

---
+++ Trabalho_2.py
@@ -0,0 +1,57 @@
+#Questão 1
+
+print("João"+"_"+str(190521))
+
+#Questão 2
+
+Nome="João"
+Idade=11
+print(Nome+str(Idade))
+
+#Questão 3
+
+Nome="João"
+Idade=11
+print(Nome+str(Idade)+"!.")
+
+#Questão 4
+
+Nome="João"
+Idade=11
+print(Nome+" "+str(Idade)+"!.")
+
+#Questão 5
+
+Nome="João Vitor Varela"
+print(Nome.upper())
+print(Nome.lower())
+
+
+
+#Questão 6
+
+Comida=" Pure. "
+print(Comida)
+Comida2="Pure!."
+print(Comida2)
+
+#Questão 7
+
+Contas=102/23
+print(Contas)
+
+#Questão 8
+
+Comida="Pure"
+print(Comida)
+print(Comida.strip()+"!")
+
+#Questão 9
+
+print("\\novo")
+
+
+
+
+
+
